cb611095ad86b2881298883bd2434cf3281b39b3
Luke Marsden <me@lukemarsden.net>
2019-12-17 09:35:36 +0000

    change region; remove vm

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  region = "us-east-1"
 }
 
 resource "aws_cloudformation_stack" "dotscience-hub" {
