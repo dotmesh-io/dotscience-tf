052776520416fd9ee15ca56c371cb656987b19b9
Luke Marsden <me@lukemarsden.net>
2020-01-30 01:11:44 +0000

    bump

--- gcp/dotscience-gcp.tf
+++ gcp/dotscience-gcp.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance" "dotscience_hub_vm" {
 
  boot_disk {
    initialize_params {
-     image = "dotscience-images/dotscience-hub-1580345071"
+     image = "dotscience-images/dotscience-hub-1580346106"
    }
  }
 
