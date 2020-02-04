b694400fb6f4ac838a9e8e2ea338171bb4ff01af
Priya Samuel <priya@dotmesh.io>
2020-02-04 13:44:27 +0000

    Add relay access key and secret

--- aws/variables.tf
+++ aws/variables.tf
@@ -177,9 +177,11 @@ variable "dotscience_domain" {
 variable "webrelay_key" {
   description = "Key from https://my.webhookrelay.com/tokens (temporarily while we migrate to in-cluster relay)"
   type = string
+  default = "b06e261f-074e-47b5-bfbe-4d6d94ccd6f4"
 }
 
 variable "webrelay_secret" {
   description = "Secret from https://my.webhookrelay.com/tokens (temporarily while we migrate to in-cluster relay)"
   type = string
+  default = "4rAW5vq0D7uN"
 }
