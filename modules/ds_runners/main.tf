02c39c9c453a70f6d851ecb3a6c9685ab673a297
Kai Davenport <kaiyadavenport@gmail.com>
2020-04-09 17:35:26 +0100

    constrain dotscience provider version

--- modules/ds_runners/main.tf
+++ modules/ds_runners/main.tf
@@ -1,6 +1,7 @@
 provider "dotscience" {
   hub_public_url = var.hub_public_url
   hub_admin_password = var.hub_admin_password
+  version = "~> 0.0.1"
 }
 
 resource "dotscience_runners" "hub-runners" {
